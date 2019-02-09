126e72f1157929fa08da3f0b2f7503e37f15b469
Patrick Kong <kongpatrick97@gmail.com>
2019-02-09 10:48:16 -0800

    updated check-answer.js

--- backend/check-answers.js
+++ backend/check-answers.js
@@ -1,4 +1,7 @@
 var matchAnswers = function(answer1, answer2) {
+	if (answer1.length == 0 || answer2.length == 0) {
+		return false;
+	}
 	answer1 = stripAll(answer1);
 	answer2 = stripAll(answer2);
 	return answer1 == answer2;
